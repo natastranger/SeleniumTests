484b45b4cb717fa2171edf0ee199892acb9b913c
natastranger <n.izotova@tut.by>
2015-01-18 23:14:19 +0300

    default style was applied

--- src/main/java/base/TestBase.java
+++ src/main/java/base/TestBase.java
@@ -7,26 +7,25 @@ import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
 
-
 /**
  * Created by HP-G62 on 15.01.2015.
  */
-public class TestBase{
+public class TestBase {
     protected WebDriver driver;
     private static final String BASE_URL = "http://www.yandex.by/";
 
-        @BeforeMethod
-        public void before() throws InterruptedException {
-            driver = new FirefoxDriver();
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-            driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-            driver.manage().timeouts().setScriptTimeout(4, TimeUnit.SECONDS);
-            driver.get(BASE_URL);
-        }
+    @BeforeMethod
+    public void before() throws InterruptedException {
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(4, TimeUnit.SECONDS);
+        driver.get(BASE_URL);
+    }
 
     @AfterMethod
-        public void after() {
-            driver.quit();
-        }
+    public void after() {
+        driver.quit();
     }
+}
